c1388ba40ce893a8497735092e1efcc33a45cfc1
Faragon02 <chj20202@naver.com>
2025-03-04 19:31:34 +0900

    ìUpdate 1

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -27,7 +27,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         Scanner scanner = new Scanner(System.in);
         Calculator<Double> calculator = new Calculator<>();
         Previous previous = new Previous();
